1e9bfa251a661f086d80c2ac4a14b6937e7fdb34
铮 <h1693374010@126.com>
2024-10-03 15:35:55 +0800

    好好好

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from os import path
 from pathlib import Path
 
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 template_folder = path.abspath('./wiki')
 
@@ -10,7 +11,15 @@ app = Flask(__name__, template_folder=template_folder)
 app.config['FREEZER_DESTINATION'] = 'public'
 app.config['FREEZER_RELATIVE_URLS'] = True
 app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
+freezer = Freezer(app)
 
+@app.cli.command()
+def freeze():
+    freezer.freeze()
+
+@app.cli.command()
+def serve():
+    freezer.run()
 
 @app.route('/')
 def home():
